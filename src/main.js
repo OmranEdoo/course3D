ce0e53b78d62eaedb0f2ddac366477bd28215fe7
OmranEdoo <omranedoo@gmail.com>
2023-02-10 18:26:05 +0100

    initialisation, la voiture bouge et la caméra suiit bien

--- src/main.js
+++ src/main.js
@@ -1,4 +1,5 @@
 import * as THREE from 'three'
+import * as YUKA from 'yuka';
 
 // NOTE: three/addons alias or importmap does not seem to be supported by Parcel, use three/examples/jsm/ instead 
 
@@ -9,75 +10,216 @@ import {
 import {
   GLTFLoader
 } from 'three/examples/jsm/loaders/GLTFLoader.js';
+import { Vector3 } from 'three';
 
 // Example of hard link to official repo for data, if needed
 // const MODEL_PATH = 'https://raw.githubusercontent.com/mrdoob/three.js/r148/examples/models/gltf/LeePerrySmith/LeePerrySmith.glb';
 
 
 // INSERT CODE HERE
+var camera, scene, renderer, vehicle, goal, keys, route, voiture;
+let initOK = false;
 
-const scene = new THREE.Scene();
-const aspect = window.innerWidth / window.innerHeight;
-const camera = new THREE.PerspectiveCamera(75, aspect, 0.1, 1000);
+var time = 0;
+var newPosition = new THREE.Vector3();
+var matrix = new THREE.Matrix4();
 
-const light = new THREE.AmbientLight(0x404040); // soft white light
-scene.add(light);
+var stop = 1;
+var DEGTORAD = 0.01745327;
+var temp = new THREE.Vector3;
+var dir = new THREE.Vector3;
+var a = new THREE.Vector3;
+var b = new THREE.Vector3;
+var coronaSafetyDistance = 5;
+var velocity = 0.0;
+var speed = 0.0;
 
-const renderer = new THREE.WebGLRenderer();
-renderer.setSize(window.innerWidth, window.innerHeight);
-document.body.appendChild(renderer.domElement);
+init();
+animate();
 
-const controls = new OrbitControls(camera, renderer.domElement);
-controls.listenToKeyEvents(window); // optional
+function init() {
 
-const geometry = new THREE.BoxGeometry(1, 1, 1);
-const material = new THREE.MeshNormalMaterial();
-const cube = new THREE.Mesh(geometry, material);
+  camera = new THREE.PerspectiveCamera(70, window.innerWidth / window.innerHeight, 0.01, 10);
+  camera.position.set(0, 3.3, 0);
 
-scene.add(cube);
+  scene = new THREE.Scene();
+  camera.lookAt(scene.position);
 
-function loadData() {
- new GLTFLoader()
-     .setPath('assets/models/')
-     .load('test.glb', gltfReader);
+  goal = new THREE.Object3D;
+  //follow = new THREE.Object3D;
+  goal.position.z = -coronaSafetyDistance;
+  goal.add(camera);
+
+  var gridHelper = new THREE.GridHelper(100, 100);
+  scene.add(gridHelper);
+
+  scene.add(new THREE.AxesHelper());
+
+  renderer = new THREE.WebGLRenderer({ antialias: true });
+  renderer.setSize(window.innerWidth, window.innerHeight);
+  document.body.appendChild(renderer.domElement);
+
+  //const controls = new OrbitControls(camera, renderer.domElement);
+  //controls.listenToKeyEvents(window); // optional
+
+  const ambientLight = new THREE.AmbientLight(0x333333);
+  scene.add(ambientLight);
+
+  const directionalLight = new THREE.DirectionalLight(0xFFFFFF, 1);
+  directionalLight.position.set(0, 10, 10);
+  scene.add(directionalLight);
+
+  var routeGeometry = new THREE.BoxBufferGeometry(1, 1, 1);
+  var routeMaterial = new THREE.MeshNormalMaterial();
+
+  route = new THREE.Mesh(routeGeometry, routeMaterial);
+
+  scene.add(route);
+
+  vehicle = new YUKA.Vehicle(); /// ?
+
+  function sync(entity, renderComponent) {
+    renderComponent.matrix.copy(entity.worldMatrix);
+  }
+
+  const path = new YUKA.Path();
+  path.add(new YUKA.Vector3(-16, 0, 4));
+  path.add(new YUKA.Vector3(-12, 0, 0));
+  path.add(new YUKA.Vector3(-6, 0, -12));
+  path.add(new YUKA.Vector3(0, 0, 0));
+  path.add(new YUKA.Vector3(8, 0, -8));
+  path.add(new YUKA.Vector3(10, 0, 0));
+  path.add(new YUKA.Vector3(4, 0, 4));
+  path.add(new YUKA.Vector3(0, 0, 6));
+
+  path.loop = true;
+
+  //vehicle.position.copy(path.current());
+
+  vehicle.maxSpeed = 5;
+
+  const followPathBehavior = new YUKA.FollowPathBehavior(path, 3);
+  //vehicle.steering.add(followPathBehavior);
+
+  const onPathBehavior = new YUKA.OnPathBehavior(path);
+  //onPathBehavior.radius = 2;
+  //vehicle.steering.add(onPathBehavior);
+
+  const entityManager = new YUKA.EntityManager();
+  //entityManager.add(vehicle);
+
+  function loadData(fileName, coord) {
+    new GLTFLoader()
+      .setPath('assets/models/')
+      .load(fileName, function (gltf) {
+        voiture = gltf.scene;//.children[0];
+
+        if (voiture != null) {
+          console.log("Model loaded:  " + voiture);
+          voiture.matrixAutoUpdate = true;
+          console.log(voiture);
+          //voiture.position.set(10, 0, 0);
+          //voiture.scale = new THREE.Vector3(20, 2, 2);
+          scene.add(voiture);
+          console.log(voiture);
+
+          initOK = true;
+          /*
+          vehicle.scale = new YUKA.Vector3(0.2, 0.2, 0.2);
+          vehicle.setRenderComponent(model, sync);*/
+
+        }
+        else {
+          console.log("Load FAILED.");
+        }
+      });
+  }
+  /////faire pas un vehicule yuka mais un objet normal 
+
+
+  loadData('voiture.glb', new Vector3(0, 0, 0));
+  //loadData('voiture.glb', new Vector3(1, 0, 1));
+
+  const positions = [];
+  for (let i = 0; i < path._waypoints.length; i++) {
+    const point = path._waypoints[i];
+    positions.push(point.x, point.y, point.z);
+  }
+
+  const lineGeometry = new THREE.BufferGeometry();
+  lineGeometry.setAttribute('position', new THREE.Float32BufferAttribute(positions, 3));
+
+  const lineMaterial = new THREE.LineBasicMaterial({ color: 0xFFFFFF });
+  const lines = new THREE.LineLoop(lineGeometry, lineMaterial);
+  scene.add(lines);
+
+  keys = {
+    a: false,
+    s: false,
+    d: false,
+    w: false
+  };
+  document.body.addEventListener('keydown', function (e) {
+
+    var key = e.code.replace('Key', '').toLowerCase();
+    if (keys[key] !== undefined)
+      keys[key] = true;
+
+  });
+  document.body.addEventListener('keyup', function (e) {
+
+    var key = e.code.replace('Key', '').toLowerCase();
+    if (keys[key] !== undefined)
+      keys[key] = false;
+
+  });
+
+  //route.position.set(0, 0, -5);
+
+  //camera.position.set(vehicle.position.x + 2, vehicle.position.y + 2, vehicle.position.z + 2);
+  console.log(voiture);
 }
 
 
-function gltfReader(gltf) {
- let testModel = null;
-
- testModel = gltf.scene;
-
- if (testModel != null) {
-     console.log("Model loaded:  " + testModel);
-    scene.add(gltf.scene);
- } else {
-     console.log("Load FAILED.  ");
- }
-}
-
-loadData();
+function animate() {
 
 
-camera.position.z = 3;
+  //console.log(voiture.position);
+
+  requestAnimationFrame(animate);
+
+  if (!initOK) {
+    return;
+  }
+
+  speed = 0.0;
 
 
-const clock = new THREE.Clock();
+  if (keys.w)
+    speed = 0.05;
+  else if (keys.s)
+    speed = -0.05;
 
- // Main loop
-const animation = () => {
+  velocity += (speed - velocity) * .3;
+  voiture.translateZ(velocity);
 
-  renderer.setAnimationLoop(animation); // requestAnimationFrame() replacement, compatible with XR 
+  if (keys.a)
+    voiture.rotateY(0.01);
+  else if (keys.d)
+    voiture.rotateY(-0.01);
 
-  const delta = clock.getDelta();
-  const elapsed = clock.getElapsedTime();
 
-  // can be used in shaders: uniforms.u_time.value = elapsed;
+  a.lerp(voiture.position, 1.9);
+  b.copy(goal.position);
 
-  cube.rotation.x = elapsed / 2;
-  cube.rotation.y = elapsed / 1;
+  dir.copy(a).sub(b).normalize();
+  const dis = a.distanceTo(b) - coronaSafetyDistance;
+  goal.position.addScaledVector(dir, dis);
+  //temp.setFromMatrixPosition(goal.matrixWorld);
+
+  //camera.position.lerp(temp, 0.2);
+  camera.lookAt(voiture.position);
+
 
   renderer.render(scene, camera);
-};
-
-animation();
+}
